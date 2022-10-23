08f2fafc4799765ee4b3943d3e5d47116d18f7b8
mondayeze <mondayeze221@gmail.com>
2022-10-23 05:33:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0' c < = '9'; c++)
+	for (c = '0'; c < = '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
@@ -21,7 +21,7 @@ int main(void)
 				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
-					putchar('');
+					putchar(',');
 				}
 			}
 		}
