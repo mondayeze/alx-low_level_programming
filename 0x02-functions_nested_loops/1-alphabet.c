19274fdd2877123dbf12e25be75963e4650b3978
mondayeze <mondayeze221@gmail.com>
2022-10-23 16:04:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabets - print alphabet of letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
