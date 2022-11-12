4c482787c2c60a5104bf9dbc85a2f3740f071017
mondayeze <mondayeze221@gmail.com>
2022-11-12 10:32:58 -0800

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_initialize_array(char *ar, int lar)
 int _checknum(char *argv[], int n)
 int main(int argc, char *argv[])
+void _is_zero(char *argv[])
 	
 #endif
